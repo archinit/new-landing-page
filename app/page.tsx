b27c8b883172bf2d45531f405bdc8fd53dadf3b2
ArchitGitHubPro <archit.7537@gmail.com>
2025-08-07 23:54:58 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return <div>
     <h1 className="bg-gradient-to-r from-teal-500 to-red-400 inline-block bg-clip-text text-transparent">
       hi from home page 
+
+      
     </h1>
   </div>
 }
