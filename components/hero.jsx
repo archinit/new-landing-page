7517a2688cbc94cf6c3d67062b7e8f6ad5e81888
ArchitGitHubPro <archit.7537@gmail.com>
2025-08-14 23:32:52 +0530

    updated

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,7 +2,9 @@ export default function Hero() {
     return <div>
         <div>
             <div>
-                
+                <div>
+                    Hey
+                </div>
             </div>
         </div>
     </div>
